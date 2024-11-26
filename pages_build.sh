89863467667c622c28dc095878a4c89399781763
Aviva Ruben <aviva@rubenfamily.com>
2024-11-25 21:54:49 -0600

    ...

--- pages_build.sh
+++ pages_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir ./bin
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ./bin
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ./bin --tag 1.36.0
 export PATH="$PATH:$(pwd)/.bin"
 
-just build
+./bin/just build
