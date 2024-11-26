6d279b778640ac16f55cd733455b1877a4f8dacf
Aviva Ruben <aviva@rubenfamily.com>
2024-11-25 20:45:13 -0600

    setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,17 @@
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import { Component } from "solid-js";
+
+const Email: Component<{address: string}> = props => 
+  <a href={`mailto:${props.address}`} title={props.address}>{props.address}</a>
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <div class=".centered">
+        <Title>Aviva Ruben</Title>
+        <h1>Aviva Ruben</h1>
+        <Email address="aviva@rubenfamily.com" />
+      </div>
     </main>
   );
 }
