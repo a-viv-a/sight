b366c048cdb4805c1d02f2eb4a94fdcd5f971a7c
Aviva Ruben <aviva@rubenfamily.com>
2024-11-29 21:03:21 -0600

    squish anim

--- src/components/DownArrow.tsx
+++ src/components/DownArrow.tsx
@@ -1,7 +1,11 @@
 import { Component } from "solid-js";
 import styles from "./DownArrow.module.css"
 
-const DownArrow: Component = (props) => <svg class={styles.downArrow} viewBox="0 0 10 10" >
-  <path d="M2,2 L5,6 L8,2" />
-</svg>
+const DownArrow: Component<{ onClick: () => void, squish: boolean }> = (props) =>
+  <svg classList={{
+    [styles.downArrow]: true,
+    [styles.squish]: props.squish
+  }} viewBox="0 0 10 10" onClick={props.onClick}>
+    <path d="M2,2 L5,6 L8,2" />
+  </svg>
 export default DownArrow
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@solidjs/meta";
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import styles from "./index.module.css"
 import DownArrow from "~/components/DownArrow";
 
@@ -7,13 +7,17 @@ const Email: Component<{ address: string }> = props =>
   <a class={styles.email} href={`mailto:${props.address}`} title={props.address}>{props.address}</a>
 
 export default function Home() {
+  const [sliding, setSliding] = createSignal(false);
+
   return (
     <main>
       <div class={styles.card}>
         <Title>Aviva Ruben</Title>
         <h1>Aviva Ruben</h1>
         <Email address="aviva@rubenfamily.com" />
-        <DownArrow />
+        <DownArrow squish={sliding()} onClick={
+          () => { setSliding(v => !v) }
+        } />
       </div>
       <div class={styles.card}>
         hi
