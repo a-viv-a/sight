6ede8e4d71c2ab639b2ef2d8521bf36a96f0e194
Aviva Ruben <aviva@rubenfamily.com>
2024-11-30 16:34:47 -0600

    fix styling add svg

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,12 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
+              <text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' font-size='85'>👁️</text>
+            </svg>"
+          />
           {assets}
         </head>
         <body>
