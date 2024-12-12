9ee67760fd329754a5b5ec580a16498f0ed14fe9
Aviva Ruben <aviva@rubenfamily.com>
2024-12-12 16:19:41 -0600

    fix gallery opengraph

--- src/routes/gallery.tsx
+++ src/routes/gallery.tsx
@@ -1,51 +1,15 @@
-import { Meta, Title } from "@solidjs/meta";
 import styles from "./index.module.css"
 import { Gallery } from "~/components/Pixel";
-
-// const paintings: Uint8Array[] = []
-
-// let paint = paintings[paintings.length] = new Uint8Array(64)
-// for (let i = 0; i < 64; i++) {
-//   paint[i] = i % 6
-// }
-// for (let p = 0; p <= 3; p++) {
-//   let paint = paintings[paintings.length] = new Uint8Array(64)
-//   for (let i = 0; i < 64; i++) {
-//     paint[i] = i / 8 % 6
-//   }
-// }
-// for (let p = 0; p <= 2; p++) {
-//   let paint = paintings[paintings.length] = new Uint8Array(64)
-//   for (let i = 0; i < 64; i++) {
-//     paint[i] = i / 3 % 6
-//   }
-// }
-// for (let p = 0; p <= 2; p++) {
-//   let paint = paintings[paintings.length] = new Uint8Array(64)
-//   for (let i = 0; i < 64; i++) {
-//     paint[i] = i % 3
-//   }
-// }
-// for (let p = 0; p <= 8; p++) {
-//   let paint = paintings[paintings.length] = new Uint8Array(64)
-//   for (let i = 0; i < 64; i++) {
-//     paint[i] = i % 2 + i % 3 + i % 4
-//   }
-// }
-// for (let p = 1; p <= 24; p++) {
-//   let paint = paintings[paintings.length] = new Uint8Array(64)
-//   for (let i = 0; i < 64; i++) {
-//     paint[i] = i % p / 4
-//   }
-// }
+import Metadata from "~/components/Metadata";
 
 export default function GalleryRoute() {
 
   return (
     <main classList={{ [styles.page]: true, [styles.padtop]: true }}>
-      <Title>Gallery</Title>
-      <Meta property="og:title" content="Gallery" />
-      <Meta property="og:description" content="Standalone page for viewing all the cool pixel art users uploaded" />
+      <Metadata
+        title="Gallery"
+        description="Standalone page for viewing all the cool pixel art users uploaded"
+      />
       <Gallery />
     </main>
   );
